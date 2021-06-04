2067385d0de070489de0c0df425b0e9fb51e7f50
Franz Heinzmann <frando@unbiskant.org>
2021-06-04 20:22:30 +0200

    remove outdated comments

--- example-jack/build.rs
+++ example-jack/build.rs
@@ -2,8 +2,5 @@ use faust_build::build_dsp;
 
 fn main() {
     println!("cargo:rerun-if-changed=dsp");
-    // build_dsp("dsp/mixer.dsp");
-    // build_dsp("dsp/volume.dsp");
     build_dsp("dsp/volume.dsp");
-    // build_dsp("dsp/passthrough.dsp");
 }
