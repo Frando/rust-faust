0f868abfa167eb8153caf2676306b7e06f5495df
Jikstra <34889164+Jikstra@users.noreply.github.com>
2022-07-23 19:40:18 +0200

    Add example showing how to get back values from faust [dbmeter] (#4)
    * Start working on dbmeter
    
    * Fix state values not getting updated

---
+++ example-dbmeter-jack/src/main.rs
@@ -0,0 +1,30 @@
+use std::{thread, time::Duration};
+
+use faust_state::DspHandle;
+
+mod jack;
+mod faust {
+    include!(concat!(env!("OUT_DIR"), "/dsp.rs"));
+}
+
+fn main() {
+    let (dsp, mut state) = DspHandle::<faust::Volume>::new();
+    eprintln!("client name: {}", dsp.name());
+    eprintln!("inputs: {}", dsp.num_inputs());
+    eprintln!("outputs: {}", dsp.num_outputs());
+    eprintln!("params: {:#?}", state.params());
+    eprintln!("meta: {:#?}", state.meta());
+
+    let mut volume = -70.;
+
+    // Spawn a thread to do state changes.
+    // This could be a GUI thread or API server.
+    thread::spawn(move || loop {
+        state.update();
+        eprintln!("volume: {:?} dB", state.get_by_path("channel0").unwrap());
+        thread::sleep(Duration::from_millis(200));
+    });
+
+    // Run the DSP as JACK client.
+    jack::run_dsp(dsp);
+}
