2131bd90432f16a6e11313c9d6a1b8e57b72e4a9
olaf <olaf@klingt.org>
2024-09-29 20:33:00 +0200

    cleanup examples

--- examples/example-f64/src/main.rs
+++ examples/example-f64/src/main.rs
@@ -6,5 +6,5 @@ fn main() {
     println!("Hello, world!");
     let mut dsp = Volume::new();
     dsp.init(44_100);
-    dsp.set_param(ParamIndex(0), 10.0 as f64);
+    dsp.set_param(ParamIndex(0), 10.0_f64);
 }
